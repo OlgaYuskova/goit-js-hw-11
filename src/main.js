57a24b3374b2a8af7946632cc8225b150c56d1f6
OlgaYuskova <132465671+OlgaYuskova@users.noreply.github.com>
2023-10-11 00:52:52 +0300

    up datesss

--- src/main.js
+++ src/main.js
@@ -4,104 +4,93 @@ const refs = {
     form: document.querySelector(".search-form"),
     gallery: document.querySelector(".gallery-img"),
     buttonMore: document.querySelector(".btn-load-more")
-}
+};
 
 let currentPage = 1;
 
 refs.buttonMore.addEventListener('click', onHandleLMBtnClick);
-refs.buttonMore.addEventListener("submit", onHandleSearchImg)
 
 function onHandleSearchImg(evt) {
     evt.preventDefault();
-    const searchInputImg = refs.form.value.trim()
+    const searchInputImg = refs.form.querySelector('input').value.trim();
     if (searchInputImg !== "") {
-    searchImg(searchInputImg)
+        searchImg(1, searchInputImg);
     }
 }
 
-async function searchImg(page = 1, q) {
+async function searchImg(page, q) {
     if (page === 1) {
-    refs.gallery.innerHTML = '';
-    currentPage = 1;
-    };
+        refs.gallery.innerHTML = '';
+        currentPage = 1;
+    }
 
-    const param = {
-    query: q,
-    page: page,
+    const params = {
+        query: q,
+        page: page,
     };
     
     try {
-    const resp = await axios.get('', { param });
-    const data = resp.data;
-    if (data.hits.length === 0) {
-      console.log(
-        'Sorry, there are no images matching your search query. Please try again.'
-      );
-    } else {
-      createMarkupImg(data.hits);
-    }
-    if (page === 1) {
-      console.log(`We found ${data.totalHits} images.`);
+        const resp = await axios.get('URL_TO_YOUR_API', { params });
+        const data = resp.data;
+        if (data.hits.length === 0) {
+            console.log('Sorry, there are no images matching your search query. Please try again.');
+        } else {
+            createMarkupImg(data.hits);
         }
-    if (refs.gallery.childElementCount < data.totalHits) {
-      unhidenMoreBtn();
-    } else {
-      hidenMoreBtn();
- 
+        if (page === 1) {
+            console.log(`We found ${data.totalHits} images.`);
+        }
+        if (refs.gallery.childElementCount < data.totalHits) {
+            unhideMoreBtn();
+        } else {
+            hideMoreBtn();
+        }
+    } catch (error) {
+        console.error(error);
     }
-  } catch (error) {
-    console.error(error);
-  }
-
-
 }
 
 function createMarkupImg(imgs) {
-  const galleryConteiner = imgs
-    .map(
-      img => `<div class="photo-card">
-        <a href="${img.largeImageURL}">
-          <img src="${img.webformatURL}" alt="${img.tags}" loading="lazy" />
-        </a>
-        <div class="info">
-          <p class="info-item"><b>Views:</b> ${img.views}</p>
-          <p class="info-item"><b>Likes:</b> ${img.likes}</p>
-          <p class="info-item"><b>Downloads:</b> ${img.downloads}</p>
-          <p class="info-item"><b>Comments:</b> ${img.comments}</p>
-        </div>
-      </div>`
-    )
-    .join('');
+    const galleryContainer = imgs.map(
+        img => `<div class="photo-card">
+            <a href="${img.largeImageURL}">
+                <img src="${img.webformatURL}" alt="${img.tags}" loading="lazy" />
+            </a>
+            <div class="info">
+                <p class="info-item"><b>Views:</b> ${img.views}</p>
+                <p class="info-item"><b>Likes:</b> ${img.likes}</p>
+                <p class="info-item"><b>Downloads:</b> ${img.downloads}</p>
+                <p class="info-item"><b>Comments:</b> ${img.comments}</p>
+            </div>
+        </div>`
+    ).join('');
 
-  refs.gallery.insertAdjacentHTML('beforeend', galleryConteiner);
-};
+    refs.gallery.insertAdjacentHTML('beforeend', galleryContainer);
+}
 
 function scrollGallery() {
-  const { height: cardHeight } =
-    refs.gallery.lastElementChild.getBoundingClientRect();
-  window.scrollBy({
-    top: cardHeight * 2,
-    behavior: 'smooth',
-  });
+    const { height: cardHeight } = refs.gallery.lastElementChild.getBoundingClientRect();
+    window.scrollBy({
+        top: cardHeight * 2,
+        behavior: 'smooth',
+    });
 }
 
 function onHandleLMBtnClick() {
-    const searchQuery = refs.form.searchQuery.value.trim();
+    const searchQuery = refs.form.querySelector('input').value.trim();
     if (searchQuery !== '') {
         currentPage += 1;
-
-        searchImg(searchQuery, currentPage);
+        searchImg(currentPage, searchQuery);
         scrollGallery();
     }
 }
 
-
-function unhidenMoreBtn() {
-  refs.buttonMore.style.display = 'block';
+function unhideMoreBtn() {
+    refs.buttonMore.style.display = 'block';
 }
 
-function hidenMoreBtn() {
-  refs.buttonMore.style.display = 'none';
+function hideMoreBtn() {
+    refs.buttonMore.style.display = 'none';
 }
 
 
