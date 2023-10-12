8e048aa797bb5599671496c1631987ba897a7a85
OlgaYuskova <132465671+OlgaYuskova@users.noreply.github.com>
2023-10-12 10:42:40 +0300

    updates

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import axios from 'axios';
 
-const axiosInstance = axios.create({
-  baseURL: 'https://pixabay.com/api/',
-  params: {
-    key: '39797585-95f120e70fb7e422bd65b56f5',
+axios.defaults.headers.common["x-api-key"] =
+  "39858338-ffd7e7633f3f70977c15ff106";
+axios.defaults.baseURL = 'https://pixabay.com/api';
+
+export function getUser() {
+
+  const params = {
+    page: 'page',
+    q: 'q',
     image_type: 'photo',
     orientation: 'horizontal',
     safesearch: true,
-    per_page: 40,
-  },
-});
-export default axiosInstance;
+    per_page: 10,
+  };
+  
+  return axios
+    .get('', { params })
+    .then((res) => res.data);
+}
--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import axios from './index';
+import { getUser } from "./index";
 
 const refs = {
     form: document.querySelector(".search-form"),
@@ -9,14 +9,7 @@ const refs = {
 let currentPage = 1;
 
 refs.buttonMore.addEventListener('click', onHandleLMBtnClick);
-
-function onHandleSearchImg(evt) {
-    evt.preventDefault();
-    const searchInputImg = refs.form.querySelector('input').value.trim();
-    if (searchInputImg !== "") {
-        searchImg(1, searchInputImg);
-    }
-}
+refs.form.addEventListener('submit', onHandleSearchImg);
 
 async function searchImg(page, q) {
     if (page === 1) {
@@ -24,14 +17,8 @@ async function searchImg(page, q) {
         currentPage = 1;
     }
 
-    const params = {
-        query: q,
-        page: page,
-    };
-    
     try {
-        const resp = await axios.get('URL_TO_YOUR_API', { params });
-        const data = resp.data;
+        const data = await getUser(page, q);
         if (data.hits.length === 0) {
             console.log('Sorry, there are no images matching your search query. Please try again.');
         } else {
@@ -59,7 +46,7 @@ function createMarkupImg(imgs) {
             <div class="info">
                 <p class="info-item"><b>Views:</b> ${img.views}</p>
                 <p class="info-item"><b>Likes:</b> ${img.likes}</p>
-                <p class="info-item"><b>Downloads:</b> ${img.downloads}</p>
+                <p class "info-item"><b>Downloads:</b> ${img.downloads}</p>
                 <p class="info-item"><b>Comments:</b> ${img.comments}</p>
             </div>
         </div>`
@@ -74,6 +61,14 @@ function scrollGallery() {
         top: cardHeight * 2,
         behavior: 'smooth',
     });
+}
+
+function onHandleSearchImg(evt) {
+    evt.preventDefault();
+    const searchInputImg = refs.form.querySelector('input').value.trim();
+    if (searchInputImg !== "") {
+        searchImg(1, searchInputImg);
+    }
 }
 
 function onHandleLMBtnClick() {
@@ -92,6 +87,99 @@ function unhideMoreBtn() {
 function hideMoreBtn() {
     refs.buttonMore.style.display = 'none';
 }
+
+
+
+// import { getUser } from "./index";
+
+// const refs = {
+//     form: document.querySelector(".search-form"),
+//     gallery: document.querySelector(".gallery-img"),
+//     buttonMore: document.querySelector(".btn-load-more")
+// };
+
+// let currentPage = 1;
+
+// refs.buttonMore.addEventListener('click', onHandleLMBtnClick);
+// refs.form.addEventListener('submit', onHandleSearchImg);
+
+
+// function onHandleSearchImg(evt) {
+//     evt.preventDefault();
+//     const searchInputImg = refs.form.querySelector('input').value.trim();
+//     if (searchInputImg !== "") {
+//         searchImg(1, searchInputImg);
+//     }
+// }
+
+// function searchImg(page) {
+//     if (page === 1) {
+//         refs.gallery.innerHTML = '';
+//         currentPage = 1;
+//     }
+
+//     try {
+
+//         if (getUser().hits.length === 0) {
+//             console.log('Sorry, there are no images matching your search query. Please try again.');
+//         } else {
+//             createMarkupImg(data.hits);
+//         }
+//         if (page === 1) {
+//             console.log(`We found ${data.totalHits} images.`);
+//         }
+//         if (refs.gallery.childElementCount < data.totalHits) {
+//             unhideMoreBtn();
+//         } else {
+//             hideMoreBtn();
+//         }
+//     } catch (error) {
+//         console.error(error);
+//     }
+// }
+
+// function createMarkupImg(imgs) {
+//     const galleryContainer = imgs.map(
+//         img => `<div class="photo-card">
+//             <a href="${img.largeImageURL}">
+//                 <img src="${img.webformatURL}" alt="${img.tags}" loading="lazy" />
+//             </a>
+//             <div class="info">
+//                 <p class="info-item"><b>Views:</b> ${img.views}</p>
+//                 <p class="info-item"><b>Likes:</b> ${img.likes}</p>
+//                 <p class="info-item"><b>Downloads:</b> ${img.downloads}</p>
+//                 <p class="info-item"><b>Comments:</b> ${img.comments}</p>
+//             </div>
+//         </div>`
+//     ).join('');
+
+//     refs.gallery.insertAdjacentHTML('beforeend', galleryContainer);
+// }
+
+// function scrollGallery() {
+//     const { height: cardHeight } = refs.gallery.lastElementChild.getBoundingClientRect();
+//     window.scrollBy({
+//         top: cardHeight * 2,
+//         behavior: 'smooth',
+//     });
+// }
+
+// function onHandleLMBtnClick() {
+//     const searchQuery = refs.form.querySelector('input').value.trim();
+//     if (searchQuery !== '') {
+//         currentPage += 1;
+//         searchImg(currentPage, searchQuery);
+//         scrollGallery();
+//     }
+// }
+
+// function unhideMoreBtn() {
+//     refs.buttonMore.style.display = 'block';
+// }
+
+// function hideMoreBtn() {
+//     refs.buttonMore.style.display = 'none';
+// }
 
 
 
